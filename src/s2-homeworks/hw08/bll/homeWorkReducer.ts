2da6062763ccf8285c02bfaadf62b16220b90164
hrsk <hrsk.yegor@gmail.com>
2024-04-04 19:24:15 +0300

    homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,25 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let stateCopy = [...state];
+            stateCopy.sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? stateCopy : stateCopy.reverse()
 
-            return state // need to fix
+            // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(user => user.age > 18) // need to fix
         }
         default:
             return state
